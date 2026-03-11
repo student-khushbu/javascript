1fca19965ab08749d6c40a77b14264af3b86c19e
student-khushbu <khushbushah7828859207@gmail.com>
2026-03-11 13:14:20 +0530

    interview first problem  solution

---
+++ interview.js
@@ -0,0 +1,18 @@
+//  1 write a javascript function that prints the letter a to z in th console. you should use a loop to iterate through the letters and print eachone on new line .
+// 2. write a function to count the number of vowels in a strings 
+// 3 .write a function to check if a all the vowel presents in a string or not  
+
+//  1 solution
+
+// for( let char = 97 ; char <= 122 ; char++){
+    // console.log(String.fromCharCode(char));
+// }
+
+// 2 .
+
+const checkAllVowelPresentOrNot = (str) => {
+    const vowels = "aeiou";
+    for (let char of vowels) {
+        console.log(char);
+    }
+};
